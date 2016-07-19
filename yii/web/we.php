4062fdd182dd75e19fac3445e498db31a0559904
MYDTX <MYDTX>
2016-07-19 12:54:51 +0800

    *

--- yii/web/we.php
+++ yii/web/we.php
@@ -32,36 +32,61 @@ class wechatCallbackapiTest
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
         //extract post data
+
         if (!empty($postStr)){
-            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-               the best way is to check the validity of xml by yourself */
-            libxml_disable_entity_loader(true);
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
             $time = time();
-            $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";
-            if(!empty( $keyword ))
+            //加载图文模版
+            $picTpl = "<xml>
+                                   <ToUserName><![CDATA[%s]]></ToUserName>
+                                   <FromUserName><![CDATA[%s]]></FromUserName>
+                                   <CreateTime>%s</CreateTime>
+                                   <MsgType><![CDATA[%s]]></MsgType>
+                                   <ArticleCount>1</ArticleCount>
+                                   <Articles>
+                                   <item>
+                                   <Title><![CDATA[%s]]></Title>
+                                   <Description><![CDATA[%s]]></Description>
+                                   <PicUrl><![CDATA[%s]]></PicUrl>
+                                   <Url><![CDATA[%s]]></Url>
+                                   </item>
+                                   </Articles>
+                                   <FuncFlag>1</FuncFlag>
+                                   </xml> ";
+            if(trim($postObj->MsgType) == "event" and trim($postObj->Event) == "subscribe")//判断是否是新关注
             {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                $msgType = "news";
+                $title = "百度"; //标题
+                $data  = date('Y-m-d'); //时间
+                $desription = "“不会就百度吧！“"; //简介
+                $image = "https://www.baidu.com/img/bd_logo1.png"; //图片地址
+                $turl = "https://www.baidu.com/index.php?tn=monline_3_dg"; //链接地址
+                $resultStr = sprintf($picTpl, $fromUsername, $toUsername, $time, $msgType, $title,$desription,$image,$turl);
+                echo $resultStr;
+            }elseif($keyword=="百度"){
+                $msgType = "news";
+                $title = "百度"; //标题
+                $data  = date('Y-m-d'); //时间
+                $desription = "“不会就百度吧！“"; //简介
+                $image = "https://www.baidu.com/img/bd_logo1.png"; //图片地址
+                $turl = "https://www.baidu.com/index.php?tn=monline_3_dg"; //链接地址
+                $resultStr = sprintf($picTpl, $fromUsername, $toUsername, $time, $msgType, $title,$desription,$image,$turl);
+                echo $resultStr;
+            }elseif(!empty($keyword )){
+                $msgType = "news";
+                $title = "张老师的博客"; //标题
+                $data  = date('Y-m-d'); //时间
+                $desription = "“张老师对php的讲解！“"; //简介
+                $image = "http://avatar.csdn.net/6/A/F/1_zph1234.jpg"; //图片地址
+                $turl = "http://blog.csdn.net/zph1234/article/details/51250523"; //链接地址
+                $resultStr = sprintf($picTpl, $fromUsername, $toUsername, $time, $msgType, $title,$desription,$image,$turl);
                 echo $resultStr;
             }else{
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
+                echo "说点什么吧!";
             }
-
         }else {
             echo "";
             exit;
